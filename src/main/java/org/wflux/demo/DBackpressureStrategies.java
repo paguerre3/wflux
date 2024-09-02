5ddf39122c5fab95447ab1cfde38dc75893a7b0a
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-02 20:37:55 -0300

    backpressure code

---
+++ src/main/java/org/wflux/demo/DBackpressureStrategies.java
@@ -0,0 +1,53 @@
+package org.wflux.demo;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public class DBackpressureStrategies {
+
+    /**
+     * There is "No Overflow" here as the data comes only after a request is pérformed, emitting is in "sequence".
+     * e.g. request(1) and then onNext(1) ...
+     *
+     * @return Flux<Long>
+     */
+    private Flux<Long> createNoOverflowFlux() {
+        // create a range with a "large" count:
+        // Build a Flux that will only emit a "sequence" of count incrementing integers, starting from start.
+        // That is, emit integers between start (included) and start + count (excluded) then complete.
+        return Flux.range(1, Integer.MAX_VALUE)
+                .log()
+                // simulate that processing takes some milliseconds:
+                .concatMap(i -> Mono.delay(Duration.ofMillis(100)));
+    }
+
+    /**
+     * "Overflow" will occur because evey "1" millisecond the data is being emitted
+     * but the processing time is larger, i.e. "100" milliseconds.
+     **
+     * @return Flux<Long>
+     */
+    private Flux<Long> createOverflowFlux() {
+        //  Emit every 1 millisecond:
+        //  Create a Flux that emits long values starting with 0 and incrementing at specified time intervals on the global timer.
+        //  The first element is emitted after an initial delay equal to the period.
+        //  If demand is not produced in time, an "onError will be signalled with an overflow IllegalStateException detailing the tick that couldn't be emitted".
+        //  In normal conditions, the Flux will never complete.
+        //  Runs on the Schedulers. parallel() Scheduler
+        return Flux.interval(Duration.ofMillis(1))
+                .log()
+                // Process time takes 100 milliseconds:
+                // simulate that processing takes some milliseconds.
+                .concatMap(i -> Mono.delay(Duration.ofMillis(100)));
+    }
+
+    public static void main(String[] args) {
+        var bs = new DBackpressureStrategies();
+        bs.createNoOverflowFlux()
+                // blockLast(): it automatically subscribes to the Flux and starts processing elements
+                // blocking indefinitely until the upstream signals its last value or completes:
+                .blockLast();
+    }
+}
