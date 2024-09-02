23721e7506e6d48572ddf0a272722a74d2bd6daa
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-01 22:23:45 -0300

    database work

--- src/main/java/org/wflux/demo/CDatabaseConfiguration.java
+++ src/main/java/org/wflux/demo/CDatabaseConfiguration.java
@@ -7,49 +7,24 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-import java.util.Collections;
-
-@Configuration
 @EnableReactiveMongoRepositories
 public class CDatabaseConfiguration extends AbstractReactiveMongoConfiguration {
-    @Value("${spring.data.mongodb.host}")
-    private String host;
 
-    @Value("${spring.data.mongodb.port}")
-    private int port;
-
-    @Value("${spring.data.mongodb.database}")
-    private String database;
-
-    @Value("${spring.data.mongodb.username}")
-    private String username;
-
-    @Value("${spring.data.mongodb.password}")
-    private String password;
-
-    @Value("${spring.data.mongodb.authentication-database}")
-    private String authDatabase;
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
     @Bean
     public MongoClient mongoClient() {
-        MongoCredential credential = MongoCredential.createCredential(username, authDatabase, password.toCharArray());
-
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyToClusterSettings(builder ->
-                        builder.hosts(Collections.singletonList(new ServerAddress(host, port))))
-                .credential(credential)
-                .build();
-        return MongoClients.create();
+        return MongoClients.create(mongoUri);
     }
 
     @Override
     protected String getDatabaseName() {
-        return database;
+        return "demo";
     }
 
     // for making reactive queries:
